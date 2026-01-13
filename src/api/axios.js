6e346c3d3eeac0c134b08bac2119ceee6bf4833d
syed643 <syed@shnoor.com>
2026-01-13 12:43:00 +0530

    c

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { auth } from "../auth/firebase";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(async (config) => {
