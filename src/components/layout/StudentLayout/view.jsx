41011af8161fd945e15bbaf01c132a4f98a5729e
syed643 <syed@shnoor.com>
2026-02-06 17:14:33 +0530

    c

--- src/components/layout/StudentLayout/view.jsx
+++ src/components/layout/StudentLayout/view.jsx
@@ -16,7 +16,7 @@ import {
 } from "lucide-react";
 import markLogo from "../../../assets/just_logo.jpeg";
 import NotificationToast from "../../common/NotificationToast";
-import StudentBot from "../../../components/StudentBot/StudentBot";
+// import StudentBot from "../../../components/StudentBot/StudentBot";
 
 const StudentLayoutView = ({
   studentName,
@@ -282,7 +282,6 @@ const StudentLayoutView = ({
             <Outlet context={{ studentName, xp }} />
           </div>
         </main>
-        <StudentBot />
       </div>
     </div>
   );
