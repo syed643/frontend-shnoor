099f67f22a594fe9b943b20a6529fc9fdea32b94
syed643 <syed@shnoor.com>
2026-02-06 18:51:31 +0530

    c

--- src/pages/student/StudentCourses/view.jsx
+++ src/pages/student/StudentCourses/view.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BookOpen, Search, Filter, ArrowRight, Library } from "lucide-react";
 import ReviewModal from "../../../components/student/ReviewModal";
 
