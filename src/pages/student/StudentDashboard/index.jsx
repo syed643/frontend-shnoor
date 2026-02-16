ad5b048cecee4b1b30a83033cd683a5df80e45dc
syed643 <syed@shnoor.com>
2026-02-16 11:28:27 +0530

    x

--- src/pages/student/StudentDashboard/index.jsx
+++ src/pages/student/StudentDashboard/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import api from "../../../api/axios";
 import StudentDashboardView from "./view";
