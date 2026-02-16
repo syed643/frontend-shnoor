f3618117400d71d51174b81377082836bd28bf73
syed643 <syed@shnoor.com>
2026-02-16 11:54:28 +0530

    c

--- src/pages/instructor/InstructorDashboard/index.jsx
+++ src/pages/instructor/InstructorDashboard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../../auth/firebase';
 import api from '../../../api/axios';
